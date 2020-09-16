5d41e8b25586d7421ad4c9f7350b9c3a179a9cc7
Ivan Nefagin <dfyz0r@list.ru>
2020-09-16 17:19:33 +0300

    feat(script.js): add currency data request, add event listener for convertBtn

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const inputAmount = document.querySelector('.converter__input[name="input-amount"]'),
+  outputAmount = document.querySelector('.converter__input[name="output-amount"]'),
+  convertBtn = document.querySelector('.js-convert');
+
+const getCurrencyData = async (url, currencyFrom = 'USD', currencyTo = 'RUB') => {
+  const response = await fetch(`${url}?base=${currencyFrom.toUpperCase()}`);
+
+  if (!response.ok) throw new Error(response.status);
+
+  const data = await response.json();
+
+  outputAmount.value = (inputAmount.value * data.rates[currencyTo]).toFixed(3);
+
+  return data;
+};
+
+getCurrencyData('https://api.exchangeratesapi.io/latest')
+  .catch(err => console.error(err));
+
+convertBtn.addEventListener('click', () => {
+  getCurrencyData('https://api.exchangeratesapi.io/latest')
+    .catch(err => console.error(err));
+});
