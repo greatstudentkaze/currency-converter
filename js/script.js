6052ce8803276dba2d79ef2d1aad61a36b38de8e
Ivan Nefagin <dfyz0r@list.ru>
2020-09-16 19:07:45 +0300

    feat(script.js): add functionality to change currency

--- js/script.js
+++ js/script.js
@@ -1,25 +1,59 @@
 'use strict';
 
-const inputAmount = document.querySelector('.converter__input[name="input-amount"]'),
-  outputAmount = document.querySelector('.converter__input[name="output-amount"]'),
+const currencyFieldset = document.querySelector('.js-currency'),
+  inputAmount = document.querySelector('.converter__input[id="input-amount"]'),
+  inputLabel = document.querySelector('.converter__label[for="input-amount"]'),
+  outputAmount = document.querySelector('.converter__input[id="output-amount"]'),
+  outputLabel = document.querySelector('.converter__label[for="output-amount"]'),
   convertBtn = document.querySelector('.js-convert');
 
-const getCurrencyData = async (url, currencyFrom = 'USD', currencyTo = 'RUB') => {
+let currencyFrom = currencyFieldset.querySelector('input[name="currency-from"]:checked'),
+  currencyTo = currencyFieldset.querySelector('input[name="currency-to"]:checked');
+
+const currencyMap = new Map([
+  ['USD', 'Доллар США (USD)'],
+  ['EUR', 'Евро (USD)'],
+  ['RUB', 'Российский рубль (RUB)'],
+]);
+
+const updateCurrencyLabel = (label, currency) => {
+  label.textContent = currencyMap.get(currency);
+};
+
+const getCurrencyData = async (currencyFrom = 'USD', currencyTo = 'RUB', url = 'https://api.exchangeratesapi.io/latest') => {
   const response = await fetch(`${url}?base=${currencyFrom.toUpperCase()}`);
 
   if (!response.ok) throw new Error(response.status);
 
   const data = await response.json();
 
-  outputAmount.value = (inputAmount.value * data.rates[currencyTo]).toFixed(3);
+  const rate = data.rates[currencyTo] ? data.rates[currencyTo] : 1;
+  outputAmount.value = (inputAmount.value * rate).toFixed(3);
 
   return data;
 };
 
-getCurrencyData('https://api.exchangeratesapi.io/latest')
+getCurrencyData(currencyFrom.value, currencyTo.value)
   .catch(err => console.error(err));
 
+currencyFieldset.addEventListener('input', evt => {
+  const target = evt.target;
+
+  if (!target.matches('input[type="radio"]')) return;
+
+  if (target.name === 'currency-from') {
+    currencyFrom = target;
+    updateCurrencyLabel(inputLabel, target.value);
+  } else if (target.name === 'currency-to') {
+    currencyTo = target;
+    updateCurrencyLabel(outputLabel, target.value);
+  }
+
+  getCurrencyData(currencyFrom.value, currencyTo.value)
+    .catch(err => console.error(err));
+});
+
 convertBtn.addEventListener('click', () => {
-  getCurrencyData('https://api.exchangeratesapi.io/latest')
+  getCurrencyData(currencyFrom.value, currencyTo.value)
     .catch(err => console.error(err));
 });
